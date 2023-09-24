948f3fd84a57508519b36f826b7c98e92e89da5c
anildev26 <anilsahudev26@gmail.com>
2023-09-24 23:59:46 +0530

    intial

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Feed from '../components/Feed';
+
+const Home = () => {
+    return(
+        <section className="w-full flex-center flex-col">
+         <h1 className="head_text text-center">
+            Discover & share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center"> AI-Powered Promts</span>
+         </h1>
+         <p className="desc text-center">
+            It's an open-source AI promting tool for modern world to discover, create and share creaative prompts.
+         </p>
+         <Feed />
+        </section>
+    )
+}
+export default Home;    
---
+++ components/PromtCard.jsx
@@ -0,0 +1,9 @@
+
+
+const PromtCard = () => {
+  return (
+    <div>PromtCard</div>
+  )
+}
+
+export default PromtCard
